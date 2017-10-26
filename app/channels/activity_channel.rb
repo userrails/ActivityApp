8240b7ee480990f62a1994b971e43d6a35c83bcf
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-10-26 14:11:54 +0545

    activity_channel added

---
+++ app/channels/activity_channel.rb
@@ -0,0 +1,10 @@
+class ActivityChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from "activity_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'events#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # following code will set the cable
+  # create channel by rails g channel activity
+  mount ActionCable.server => '/cable'
 end
