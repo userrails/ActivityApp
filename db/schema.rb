2c1b421462e2eee1bdd70d6d095ff2b85a7da953
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-10-26 14:24:15 +0545

    comment scaffold created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026073758) do
+ActiveRecord::Schema.define(version: 20171026083830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "message"
