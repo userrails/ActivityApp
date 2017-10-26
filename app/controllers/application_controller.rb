457fb8ff003dd3fb10dda4d188ff93841b131ec8
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-10-26 18:53:22 +0545

    count unseen events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :detect_count
+  before_action :count_unseen_event
 
-  def detect_count
+  def count_unseen_event
     @count = Event.unseen.count
   end
 end
